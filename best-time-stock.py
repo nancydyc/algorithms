e7bcb7c86b3a61a9175773fd2856dea39fb41042
Yichen Dai <msdaiyichen@gmail.com>
2020-04-13 19:28:59 +0000

    Created a file for best time to buy and sell stock challenge

--- best-time-stock.py
+++ best-time-stock.py
@@ -4,3 +4,15 @@
     You may complete as many transactions as you like
     (i.e., buy one and sell one share of the stock multiple times).
 """
+
+def maxProfit(self, prices: List[int]) -> int:
+    """Input an array prices and find the max profit.
+
+       For example:
+       >>>[7,1,5,3,6,4]
+       7
+       >>>[1,2,3,4,5]
+       4
+    """
+
+
