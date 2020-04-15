36e2aab0f28313f909b3b1b8218660178cd992f6
Yichen Dai <msdaiyichen@gmail.com>
2020-04-15 01:08:25 +0000

    Add a new file for countElements challenge

---
+++ counting-elements.py
@@ -0,0 +1,14 @@
+def countElements(arr):
+    """Given an integer array arr, count element x such that x + 1 is also in arr.
+       If there're duplicates in arr, count them seperately.
+
+       Example:
+       >>> countElements([1,2,3])
+       2
+       >>> countElements([1,1,3,3,5,5,7,7])
+       0
+       >>> countElements([1,3,2,3,5,0])
+       3
+       >>> countElements([1,1,2,2])
+       2
+    """
