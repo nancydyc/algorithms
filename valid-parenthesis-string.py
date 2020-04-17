07d71465202bc5926db0f774a7aaaff8b0aaf034
Yichen Dai <msdaiyichen@gmail.com>
2020-04-17 04:09:10 +0000

    Drafted out solution

--- valid-parenthesis-string.py
+++ valid-parenthesis-string.py
@@ -16,9 +16,24 @@ def checkValidString(str):
        True
        >>> checkValidString("(*))")
        True
+       >>> checkValidString("(*))((")
+       False
     """
 
-    #
+    # check the string from the beginning
+    # record the num of "(" until next char is not "("
+    # if the next char is ")" or "*"
+    # count their number until next char is not ")" or "*"
+    # if left_par_num - right_par_num = 0,
+    # repeat the process until the string ends, return True
+    # if num is not 0, check if num < 0 or num > 0
+    # if num > 0
+    # - if num - star_num != 0, return false
+    # - else, keep tracking until the string ends, return True
+    # if num < 0
+    # - if num + star_num != 0, return false
+    # - else, continue tracking till string ends, return True
+
 
 
 
