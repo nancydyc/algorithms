deabefef3e8d90b543e37caa34cf67308bc64832
Yichen Dai <msdaiyichen@gmail.com>
2020-04-20 20:31:05 +0000

    Use maths way to solve the problem

--- get_products_of_all_ints_except.py
+++ get_products_of_all_ints_except.py
@@ -7,3 +7,22 @@ def get_products_of_all_ints_except_at_index(integers):
        >>> get_products_of_all_ints_except_at_index([1, 7, 3, 4])
        [84, 12, 28, 21]
     """
+
+    total = 1
+
+    for num in integers:
+        total *= num
+
+    products = []
+
+    for i in integers:
+        products.append(int(total / i))
+
+    return products
+
+#######################################################
+if __name__ == "__main__":
+    import doctest
+
+    if doctest.testmod().failed == 0:
+        print("\n*** ALL TESTS PASSED. YOU CAUGHT ALL THE STRAY PARENS!\n")
