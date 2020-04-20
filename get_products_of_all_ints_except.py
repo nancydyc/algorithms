cf7fb48d443247f1c05dcfd6e29f29adcdac3d22
Yichen Dai <msdaiyichen@gmail.com>
2020-04-20 19:21:08 +0000

    Add a new file to practice greedy algo

---
+++ get_products_of_all_ints_except.py
@@ -0,0 +1,9 @@
+def get_products_of_all_ints_except_at_index(integers):
+    """Takes a list of integers and returns a list of the products.
+       for each index you want to find the product of every integer
+       except the integer at that index.
+
+       For example:
+       >>> get_products_of_all_ints_except_at_index([1, 7, 3, 4])
+       [84, 12, 28, 21]
+    """
