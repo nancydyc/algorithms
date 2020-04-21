c27ba41f0612d521006b55cdb9c0de03a70e4781
Yichen Dai <msdaiyichen@gmail.com>
2020-04-21 04:50:24 +0000

    Created a second solution for O(n) runtime

--- get_products_of_all_ints_except.py
+++ get_products_of_all_ints_except.py
@@ -7,18 +7,45 @@ def get_products_of_all_ints_except_at_index(integers):
        >>> get_products_of_all_ints_except_at_index([1, 7, 3, 4])
        [84, 12, 28, 21]
     """
+    # solution 1: using division
+    # total = 1
 
-    total = 1
+    # for num in integers:
+    #     total *= num
 
-    for num in integers:
-        total *= num
+    # products = []
 
+    # for i in integers:
+    #     products.append(int(total / i))
+
+    # return products
+
+
+    #solution 2: using greedy algo
+    first_half_products = [1]
+    second_half_products = [1]
     products = []
 
-    for i in integers:
-        products.append(int(total / i))
+    initial_product = 1
+    last_product = 1
+
+    for x in range(len(integers)-1):
+        initial_product *= integers[x]
+        first_half_products.append(initial_product)
+
+    rev_ints = integers[::-1]
+
+    for y in range(len(rev_ints)-1):
+        last_product *= rev_ints[y]
+        second_half_products.append(last_product)
+
+    second_half_products.reverse()
+
+    for x, y in zip(first_half_products, second_half_products):
+        products.append(x*y)
 
     return products
+
 
 #######################################################
 if __name__ == "__main__":
