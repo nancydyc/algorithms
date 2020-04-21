4cb7aa05cbf190ad787620c00af7efb2dab208a1
Yichen Dai <msdaiyichen@gmail.com>
2020-04-21 05:38:19 +0000

    Modified solution for O(n) space

--- get_products_of_all_ints_except.py
+++ get_products_of_all_ints_except.py
@@ -8,7 +8,10 @@ def get_products_of_all_ints_except_at_index(integers):
        [84, 12, 28, 21]
        >>> get_products_of_all_ints_except_at_index([1, 0, 3, 4])
        [0, 12, 0, 0]
+       >>> get_products_of_all_ints_except_at_index([1, 2])
+       [2, 1]
     """
+
     # solution 1: using division doesn't work for list containing 0
     # total = 1
 
@@ -23,35 +26,55 @@ def get_products_of_all_ints_except_at_index(integers):
     # return products
 
 
-    #solution 2: using greedy algo
-    first_half_products = [1]
-    second_half_products = [1]
-    products = []
+    # solution 2: using greedy algo
+    # first_half_products = [1]
+    # second_half_products = [1]
+    # products = []
 
-    initial_product = 1
-    last_product = 1
+    # initial_product = 1
+    # last_product = 1
 
-    for x in range(len(integers)-1):
-        initial_product *= integers[x]
-        first_half_products.append(initial_product)
+    # for x in range(len(integers)-1):
+    #     initial_product *= integers[x]
+    #     first_half_products.append(initial_product)
 
-    rev_ints = integers[::-1]
+    # rev_ints = integers[::-1]
 
-    for y in range(len(rev_ints)-1):
-        last_product *= rev_ints[y]
-        second_half_products.append(last_product)
+    # for y in range(len(rev_ints)-1):
+    #     last_product *= rev_ints[y]
+    #     second_half_products.append(last_product)
 
-    second_half_products.reverse()
+    # second_half_products.reverse()
 
-    for x, y in zip(first_half_products, second_half_products):
-        products.append(x*y)
+    # for x, y in zip(first_half_products, second_half_products):
+    #     products.append(x*y)
+
+    # return products
+
+
+    # Modify solution 2 to be O(n) space
+    if len(integers) < 2:
+        raise IndexError('Getting the product of numbers at other '
+                          'indices requires at least 2 numbers')
+
+    products = [None] * len(integers)
+
+    product = 1
+    for i in range(len(integers)):
+        products[i] = product
+        product *= integers[i]
+
+    product = 1
+    for i in range(len(integers) - 1, -1, -1):
+        # print(product, products[i])
+        products[i] *= product
+        product *= integers[i]
 
     return products
-
 
 #######################################################
 if __name__ == "__main__":
     import doctest
 
     if doctest.testmod().failed == 0:
-        print("\n*** ALL TESTS PASSED. YOU CAUGHT ALL THE STRAY PARENS!\n")
+        print("\n*** ALL TESTS PASSED. YOU DID AMAZING JOB!\n")
