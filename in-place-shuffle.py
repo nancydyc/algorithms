5af7b62173b541d997f233e043b0be2e7a2fcb7b
Yichen Dai <msdaiyichen@gmail.com>
2020-04-23 01:19:14 +0000

    Explored Fisher-Yates Shuffle for a O(1) space solution

--- in-place-shuffle.py
+++ in-place-shuffle.py
@@ -1,5 +1,9 @@
 import random
 
+def get_random(floor, ceiling):
+    # ceiling is exclusive
+    return random.randrange(floor, ceiling + 1)
+
 def shuffle_in_place(lst):
     """Given a list of random integers among the range of floor and ceiling numbers,
        randomly pick an integer one by one and rearrange them in order.
@@ -11,15 +15,32 @@ def shuffle_in_place(lst):
     # append the number to the end of the original list
     # repeat until sublist becomes empty
 
-    sublist = lst[:]
-    while sublist:
-        # print(sublist)
-        num = random.sample(sublist, 1)[0]
-        # print(num)
-        lst.remove(num)
+    # MY Solution
 
-        lst.append(num)
+    # sublist = lst[:]
+    # while sublist:
+    #     # print(sublist)
+    #     num = random.sample(sublist, 1)[0]
+    #     # print(num)
+    #     lst.remove(num)
 
-        sublist.remove(num)
+    #     lst.append(num)
+
+    #     sublist.remove(num)
+
+    # return lst
+
+
+    # Solution: Fisher-Yates Shuffle/Knuth Shuffle Algorithm
+
+    if len(lst) <= 1:
+        return lst
+    # search through the index for a random index instead of a random number
+    for i in range(0, len(lst) - 1):
+        random_index = get_random(i, len(lst) - 1)
+
+        if random_index != i:
+            lst[i], lst[random_index] = \
+            lst[random_index], lst[i]
 
     return lst
