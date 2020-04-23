6dbbc7fd9bdcb6be052d0aad57cc2b6a4f0c5bda
Yichen Dai <msdaiyichen@gmail.com>
2020-04-23 19:58:48 +0000

    Add a file to practice memoization

---
+++ makeing-changes.py
@@ -0,0 +1,18 @@
+def make_changes(amount, lst):
+    """Given an amount of money and a list of coin denominations, computes the number of ways
+       to make the amount of money with coins of the available denominations.
+
+       For example:
+       >>> make_changes(4, [1, 2, 3])
+       4
+    """
+
+
+
+
+#######################################################
+if __name__ == "__main__":
+    import doctest
+
+    if doctest.testmod().failed == 0:
+        print("\n*** ALL TESTS PASSED. YOU DID AMAZING JOB!\n")
