3e20f82054523a0d58d6fea39567f92be4513952
Yichen Dai <msdaiyichen@gmail.com>
2020-04-25 00:06:33 +0000

    Completed a draft solution; sure there's bug for other testcases

--- makeing-changes.py
+++ makeing-changes.py
@@ -7,7 +7,56 @@ def make_changes(amount, lst):
        4
     """
 
+    lst.sort()
 
+    ways = 0
+    memo = set()
+    for n in range(len(lst)):
+        # print(lst[n])
+        if lst[n] > amount:
+            continue
+
+        times = amount // lst[n]
+        remainder = amount % lst[n]
+        # print(times, remainder)
+        if remainder == 0:
+            ways += 1
+            combo = [lst[n]] * times
+            c = tuple(combo)
+            memo.add(c)
+
+        if remainder != 0 and remainder in lst:
+
+            combo = [lst[n]] * times
+            combo.append(remainder)
+            c = tuple(combo)
+            if c in memo:
+                continue
+            ways += 1
+            memo.add(c)
+
+        t = 1
+        for i in lst[n+1:]:
+            divider = 1
+            r = 1
+            while divider > 0 and r > 0:
+                divider = (amount - lst[n] * t) // i
+                r = (amount - lst[n] * t) % i
+                t += 1
+            # when next number involved, look for the least common multiple
+            if divider == 0:
+                continue
+
+            if r == 0:
+                combo = [lst[n]] * t
+                combo.append(i)
+                c = tuple(combo)
+                if c in memo:
+                    continue
+                ways += 1
+                memo.add(c)
+
+    return ways
 
 
 #######################################################
