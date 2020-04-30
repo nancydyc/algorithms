c64e556288cae2d538df0b3188b30b4941da24a6
Yichen Dai <msdaiyichen@gmail.com>
2020-04-30 01:49:15 +0000

    Figured out a bottom-up solution for cake problem

--- cake.py
+++ cake.py
@@ -7,11 +7,31 @@ def max_duffel_bag_value(cakes, capacity):
        555
     """
 
-    # sort the cakes' value per kg
-    # Within the capacity, take the most valuable cake
-    # for the rest capacity, try the next valuable cake
+    # add a cake then the current weight of the bag is added, so index increases
+    # at the index of the new weight, the cake's value is added to the current value as well
+    # if used another type of cake and reached the same new weight
+    # the current value will be updated to new value if current value is less than the potential new vaule
 
-    value_lst = []
+    # what if [(7, 210), (3, 90)]
+
+    values = [i * 0 for i in range(capacity + 1) ]
+    # print(len(values))
+    for cake in cakes:
+
+        weight_in_bag = 0
+        current_value = 0
+        new_value = 0
+        for weight in range(1, capacity + 1):
+            if weight % cake[0] == 0:
+                weight_in_bag = weight - cake[0]
+                new_value = values[weight_in_bag] + cake[1]
+            #     print(current_value)
+                if new_value >= values[weight]:
+                    values[weight] = new_value
+
+    return values[-1]
+
+
 
 
 
