fc4cbe54581aecaab983fce5c6581f7320f8e861
Yichen Dai <msdaiyichen@gmail.com>
2020-05-04 19:59:31 +0000

    Explored alternative solution: get first two arrays intersection then use it with the third array to get final result

--- intersection-three-sorted.py
+++ intersection-three-sorted.py
@@ -6,28 +6,52 @@ def arraysIntersection(arr1, arr2, arr3):
        >>> arraysIntersection([1,2,3,4,5], [1,2,5,7,9], [1,3,4,5,8])
        [1, 5]
     """
-    i = 0
-    j = 0
-    k = 0
-    intersection = []
-    while i < len(arr1) and j < len(arr2) and k < len(arr3):
-        if arr1[i] == arr2[j] and arr1[i] == arr3[k]:
-            intersection.append(arr1[i])
-            i += 1
-            j += 1
-            k += 1
-            continue
+    # i = 0
+    # j = 0
+    # k = 0
+    # intersection = []
+    # while i < len(arr1) and j < len(arr2) and k < len(arr3):
+    #     if arr1[i] == arr2[j] and arr1[i] == arr3[k]:
+    #         intersection.append(arr1[i])
+    #         i += 1
+    #         j += 1
+    #         k += 1
+    #         continue
 
-        if arr1[i] < arr2[j] or arr1[i] < arr3[k]:
-            i += 1
+    #     if arr1[i] < arr2[j] or arr1[i] < arr3[k]:
+    #         i += 1
 
-        if arr1[i] > arr2[j] or arr3[k] > arr2[j]:
-            j += 1
+    #     if arr1[i] > arr2[j] or arr3[k] > arr2[j]:
+    #         j += 1
 
-        if arr1[i] > arr3[k] or arr2[j] > arr3[k]:
-            k += 1
+    #     if arr1[i] > arr3[k] or arr2[j] > arr3[k]:
+    #         k += 1
 
-    return intersection
+    # return intersection
+
+    # Solution: Get the intersection of first two arrays and then use the result to get the intersection with the third array.
+
+    def get_common(a1, a2):
+        i = j = 0
+        intersection = []
+        while i < len(a1) and j < len(a2):
+            if a1[i] == a2[j]:
+                intersection.append(a1[i])
+                i += 1
+                j += 1
+
+            elif a1[i] < a2[j]:
+                i += 1
+
+            else:
+                j += 1
+        return intersection
+
+    common_a1_a2 = get_common(arr1, arr2)
+
+    return get_common(common_a1_a2, arr3)
+
+
 
 
 ############################################################################
