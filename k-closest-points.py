11e03dc2c958776c0036bcc2d3c59ca31cd5de6e
Yichen Dai <msdaiyichen@gmail.com>
2020-05-06 05:15:08 +0000

    Add one more solution of using Hoare's partition but failed on last two testcases

--- k-closest-points.py
+++ k-closest-points.py
@@ -8,6 +8,12 @@ def kClosest(points, K):
 
     >>> kClosest([[3,3],[5,-1],[-2,4]], 2)
     [[3, 3], [-2, 4]]
+
+    >>> kClosest([[-2,-5],[8,5],[-10,-3],[-7,-1],[2,-2],[2,8]], 5)
+    [[2, -2], [-2, -5], [-7, -1], [2, 8], [8, 5]]
+
+    >>> kClosest([[6,-7],[-9,-3],[10,6],[-5,-8],[-4,-10],[-3,-10],[2,-4]], 6)
+    [[2, -4], [6, -7], [-5, -8], [-9, -3], [-3, -10], [-4, -10]]
     """
     # Solution: hashmap
 
@@ -32,29 +38,62 @@ def kClosest(points, K):
     # return closest_points_lst[:K]
 
 
-    # Solution: quicksort
+    # Solution: quicksort -- Lomuto's partition
+    # def distance(points, index):
+    #     return points[index][0] ** 2 + points[index][1] ** 2
+
+    # def helper(lst, start, end):
+    #     if start >= end:
+    #         return
+
+    #     p = random.randint(start, end)
+    #     pivot = distance(lst, p)
+    #     lst[start], lst[p] = lst[p], lst[start]
+    #     smaller = start
+    #     for bigger in range(start + 1, end + 1):
+    #         if distance(lst, bigger) <= distance(lst, start):
+    #             smaller += 1
+    #             lst[bigger], lst[smaller] = lst[smaller], lst[bigger]
+
+    #     lst[start], lst[smaller] = lst[smaller], lst[start]
+
+    #     helper(lst, start, smaller - 1)
+    #     helper(lst, smaller + 1, bigger)
+
+    # sorted_points = helper(points, 0, len(points) - 1)
+
+    # return points[:K]
+
+
+    # Solution: quicksort Hoare's partition
     def distance(points, index):
         return points[index][0] ** 2 + points[index][1] ** 2
 
     def helper(lst, start, end):
-        if start >= end:
-            return
-
+        smaller = start - 1
+        bigger = end + 1
         p = random.randint(start, end)
-        pivot = distance(lst, p)
-        lst[start], lst[p] = lst[p], lst[start]
-        smaller = start
-        for bigger in range(start + 1, end + 1):
-            if distance(lst, bigger) <= distance(lst, start):
+        while True:
+            smaller += 1
+            while distance(lst, smaller) < distance(lst, p):
                 smaller += 1
-                lst[bigger], lst[smaller] = lst[smaller], lst[bigger]
 
-        lst[start], lst[smaller] = lst[smaller], lst[start]
+            bigger -= 1
+            while distance(lst, bigger) > distance(lst, p):
+                bigger -= 1
 
-        helper(lst, start, smaller - 1)
-        helper(lst, smaller + 1, bigger)
+            if smaller >= bigger:
+                return bigger
 
-    sorted_points = helper(points, 0, len(points) - 1)
+            lst[smaller], lst[bigger] = lst[bigger], lst[smaller]
+
+    def quicksort(lst, start, end):
+        if start < end:
+            partition_index = helper(lst, start, end)
+            helper(lst, start, partition_index)
+            helper(lst, partition_index + 1, end)
+
+    quicksort(points, 0, len(points) - 1)
 
     return points[:K]
 
