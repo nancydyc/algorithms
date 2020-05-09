67da53d2901ea33f96d913c644c26f1742af93c5
Yichen Dai <msdaiyichen@gmail.com>
2020-05-09 23:06:07 +0000

    Create three pointers to solve dutch-flag-problem in place and linear time

--- dutchflag.py
+++ dutchflag.py
@@ -7,3 +7,41 @@
 """
 
 def arrange(lst):
+    """Sort an array of 0, 1 and 2's but you must do this in place, in linear time and without any extra space.
+
+       For example:
+       >>> arrange([2,0,0,1,2,1])
+       [0, 0, 1, 1, 2, 2]
+
+       >>> arrange([0,1,0,2,0,1,2])
+       [0, 0, 0, 1, 1, 2, 2]
+    """
+
+    lo = 0
+    mid = 0
+    hi = len(lst) - 1
+
+    while mid <= hi:
+        if lst[mid] == 0:
+            lst[lo], lst[mid] = lst[mid], lst[lo]
+            mid += 1
+            lo += 1
+
+        elif lst[mid] == 2:
+            lst[hi], lst[mid] = lst[mid], lst[hi]
+            hi -= 1
+            # mid += 1
+
+        else:
+            mid += 1
+
+    return lst
+
+
+############################################################################
+if __name__ == "__main__":
+    import doctest
+
+    if doctest.testmod().failed == 0:
+        print("\n*** ALL TESTS PASSED. YOU DID AN EXCELLENT JOB!\n")
+
