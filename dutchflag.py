3a18fef454f2dd8361f0155758946c57b90f63ba
Yichen Dai <msdaiyichen@gmail.com>
2020-05-09 20:24:11 +0000

    Add a file for Dutch National Flag problem

---
+++ dutchflag.py
@@ -0,0 +1,9 @@
+"""
+    The flag of the Netherlands consists of three colors: red, white and blue.
+    Given balls of these three colors arranged randomly in a line (the actual number of balls does not matter),
+    the task is to arrange them such that all balls of the same color are together
+    and their collective color groups are in the correct order
+
+"""
+
+def arrange(lst):
