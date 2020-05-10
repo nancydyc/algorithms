1be37dd2ff925e0d3705d5e77a6945de405f8024
Yichen Dai <msdaiyichen@gmail.com>
2020-05-10 01:27:18 +0000

    Added a file of calculating min meeting rooms required problem; testcase added

---
+++ meetingRooms.py
@@ -0,0 +1,13 @@
+def minMeetingRooms(lst):
+    """Given an array of meeting time intervals consisting of start and end times [[s1,e1],[s2,e2],...] (si < ei),
+       find the minimum number of conference rooms required.
+
+       For example,
+
+       >>> minMeetingRooms([[0, 30],[5, 10],[15, 20]])
+       2
+
+       >>> minMeetingRooms([[7,10],[2,4]])
+       1
+    """
+
