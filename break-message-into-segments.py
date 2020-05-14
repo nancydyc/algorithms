21cd7643050bb6243f33d592e5c4c38f51910584
Yichen Dai <msdaiyichen@gmail.com>
2020-05-14 20:45:40 +0000

    Solved the break_into_segments; can improve the break point to avoid break at the mid of the word

--- break-message-into-segments.py
+++ break-message-into-segments.py
@@ -15,17 +15,30 @@ def break_into_segments(message):
         >>> break_into_segments('Ya, ya me está gustando más de lo normal. Todos mis sentidos van pidiendo más. Esto hay que tomarlo sin ningún apuro. Despacito Quiero respirar tu cuello despacito. Deja que te diga cosas al oído. Para que te acuerdes si no estás conmigo.')
         'Ya, ya me está gustando más de lo normal. Todos mis sentidos van pidiendo más. Esto hay que tomarlo sin ningún apuro. Despacito Quiero respirar tu cuello d (1/2)'
         'espacito. Deja que te diga cosas al oído. Para que te acuerdes si no estás conmigo. (2/2)'
+
+        >>> break_into_segments('Hey, I can do it!')
+        'Hey, I can do it!'
     """
 
-    # figure out how many segments the message will be split into
+    # figure out how many segments(m) the message will be split into
+    if len(message) <= 160:
+        return message
 
+    # split the message at 154 characters, leaving space for suffixes
+    sub_messages = []
+    while message:
+        segment = message[:154]
+        sub_messages.append(segment)
+        message = message.replace(message[:154], '')
 
-    # split the message
-
-    # iterate through the sub-messages and add suffixes
+    # iterate through the sub-messages and add suffixes ' (n/m)''
+    result = []
+    for n in range(1, len(sub_messages) + 1):
+        segment = sub_messages[n-1] + f' ({n}/{len(sub_messages)})'
+        result.append(segment)
 
     # join the list of sub-messages into a string of multiple lines
-
+    print('\n'.join(result))
 
 
 
