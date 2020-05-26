bfefd0623a800154b18e4910a4c9e7d410f0ba4a
Yichen Dai <msdaiyichen@gmail.com>
2020-05-26 19:48:01 +0000

    Iterative solution and recusive solution of reveselinkedlist

--- reversell.py
+++ reversell.py
@@ -2,6 +2,53 @@
 Input: 1->2->3->4->5->NULL
 Output: 5->4->3->2->1->NULL
 """
-class LinkedListNode():
+class Node():
+    def __init__(self, value):
+        self.value = value
+        self.next = None
 
-def reverse_ll(ll):
+class LinkedList():
+    def __init__(self):
+        self.head = None
+
+    def reverse_ll(head):
+        """Given a singly linked list, return its reversed list. """
+
+        # put each node to a new list
+        new_list = []
+        while head.next:
+            new_list.append(head.next.value)
+        print(new_list)
+        # pop the list from the end to make it a new ll
+        reverse_ll = LinkedList()
+        while new_list:
+            if reverse_ll.head == None:
+                new_val = new_list.pop()
+                new_node = Node(new_val)
+            else:
+                new_val = new_list.pop()
+                new_node.next = Node(new_val)
+
+        return reverse_ll
+
+# Solutions
+
+def reverseList(head):  # Iterative
+    prev, curr = None, head
+    while curr:
+        curr.next, prev, curr = prev, curr, curr.next
+    return prev
+
+def reverseList_v1(head):  # Recursive
+    """
+    :type head: ListNode
+    :rtype: ListNode
+    """
+    if not head or not head.next: # when node is none
+        return head
+    p = self.reverseList(head.next)
+    head.next.next = head # head.next = current -> current.next = head
+    head.next = None # tail is none
+    return p
+
+
