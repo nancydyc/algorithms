9bf0b3c3a5e98c12f908de8b4526f193558a751e
Yichen Dai <msdaiyichen@gmail.com>
2020-06-05 06:12:08 +0000

    Wrote a function to calculate total right values including the current node

--- bst-to-greater-tree.py
+++ bst-to-greater-tree.py
@@ -1,4 +1,28 @@
-def convertBST(self, TreeNode):
+def right_total_value(node):
+    total = 0
+    nodes = [root]
+    # print(nodes)
+    while nodes:
+        cur = nodes.pop()
+        # print(cur.val)
+
+        if cur != node and cur.val > node.val:
+            # print(total)
+            # return total + cur.val
+            total += cur.val
+
+        if cur.right and cur.right.val > node.val:
+            nodes.append(cur.right)
+
+        if cur.left and cur.left.val > node.val:
+            nodes.append(cur.left)
+
+    return total + node.val
+
+# current_total = right_total_value(root)
+# print('root\'s new value', current_total)
+
+def convertBST(self, root):
     """
     Given a Binary Search Tree (BST), convert it to a Greater Tree such that every key of the original BST
     is changed to the original key plus sum of all keys greater than the original key in BST.
